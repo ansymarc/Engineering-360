a293b6bd1c0aff04435b06cfe489d534eea01741
ansymarc <laguerremarc@gmail.com>
2025-07-24 22:03:56 -0500

    Add files via upload

---
+++ Loterie_Probabilité.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as patches
+
+# Parameters
+number_range = 100
+n_play = 50
+chosen_numbers = np.random.choice(number_range, size=n_play, replace=False)
+n_simulations = 20000
+
+
+
+# Lottery simulation function
+def simulate_lottery():
+    results = np.random.randint(0, number_range, size=3)
+    win = 0
+    if results[0] in chosen_numbers: win += 50
+    if results[1] in chosen_numbers: win += 20
+    if results[2] in chosen_numbers: win += 10
+    return win, results
+
+
+
+wins = []
+
+for i in range(0, n_simulations):
+    reward, numbers = simulate_lottery()
+    is_benefit = reward > n_play
+    wins.append(is_benefit)
+    
+    
+prob = sum(wins) / len(wins)
+n_wins = sum(wins)
+
+
+print(prob)
+
+
+
+
+
+
+
